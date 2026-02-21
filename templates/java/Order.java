ed24f3e82a10391fa066ad36ce46ef2397a79d2f
majid-shokrgozar <majidshokrgozar@gmail.com>
2026-02-21 13:29:54 +0330

    Create Order.java file with Order entity documentation

---
+++ templates/java/Order.java
@@ -0,0 +1,16 @@
+package templates.java;
+
+/**
+ * Order entity class.
+ * This class represents an order in the system.
+ *
+ * Date Created: 2026-02-21
+ * Last Modified: 2026-02-21
+ */
+public class Order {
+    private String orderId;
+    private String customerId;
+    private double totalAmount;
+
+    // Constructors, getters, setters, and other methods can be added here
+}
